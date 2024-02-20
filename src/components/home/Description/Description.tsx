b8cca200e10b39b535a27286b99d273407aa4772
CinLo82 <cinlos3382@gmail.com>
2024-02-20 19:11:35 -0300

    image management and optimization

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,7 +1,19 @@
+import Image from 'next/image'
+import styles from './Description.module.sass'
+
 export const Description = () => {
     return(
-        <section>
-            <h1>Descripción</h1>
+        <section className={styles.Description}>
+            <Image 
+                src="/images/product1.jpg" 
+                alt="product marketplace" 
+                width={500}
+                height={300}
+            />
+            <div>
+                <h2>Description</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis beatae molestias nihil culpa hic corporis, nemo doloribus nesciunt. Autem dolor sit quo ipsa expedita, nemo qui doloribus animi vero! Natus.</p>
+            </div> 
         </section>
     )
 }
