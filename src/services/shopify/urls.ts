5bb3b216c50a5bff508bf48fef2ae4b4ece3ee28
CinLo82 <cinlos3382@gmail.com>
2024-04-08 18:34:52 -0300

    data fetching patterns in nextjs

--- src/services/shopify/urls.ts
+++ src/services/shopify/urls.ts
@@ -3,5 +3,8 @@ import { env } from 'app/config/env'
 export const shopifyUrls = {
     products: {
         'all': `https://${env.SHOPIFY_HOSTNAME}/admin/api/2024-04/products.json?`
+    },
+    collections: {
+        'all': `https://${env.SHOPIFY_HOSTNAME}/admin/api/2024-04/smart_collections.json?`
     }
 }
